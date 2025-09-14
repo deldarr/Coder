05be860ca89d995acb1a3a3ff2d9ff56457bc23d
deldar <deldarr06@gmail.com>
2025-09-15 00:20:03 +0330

    create user input system

---
+++ main.py
@@ -0,0 +1,17 @@
+print('For coding, enter 1')
+print('For decoding, enter 2')
+
+user_input = input(': ')
+if user_input != '1' and user_input != '2':
+    print('Invalid input')
+    exit()
+
+if user_input == '1':
+    pass
+
+elif user_input == '2':
+    pass
+
+
+
+
