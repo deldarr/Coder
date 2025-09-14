f07203589996f7962a2328a2d5cfb93c1ec6d690
deldar <deldarr06@gmail.com>
2025-09-15 00:47:12 +0330

    complete coder function

--- coder.py
+++ coder.py
@@ -1,2 +1,19 @@
 def Coder(text):
-    pass
+
+    # convert characters to ascii code
+    ascii_list = list(map(ord, text))
+
+    #convert int list to string list
+    ascii_list = map(str, ascii_list)
+
+    # join list item with 98(_) to each other and make a number
+    code = '98'.join(ascii_list)
+    number = int(code)
+
+    output = coder_number(number)
+    return output
+
+
+
+def coder_number(number):
+    return number
