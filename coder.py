e070b00bae4a306a73b05f153dfe2c8a9b5bcd1a
deldar <deldarr06@gmail.com>
2025-09-15 01:39:48 +0330

    update Coder function

--- coder.py
+++ coder.py
@@ -6,8 +6,8 @@ def Coder(text):
     #convert int list to string list
     ascii_list = map(str, ascii_list)
 
-    # join list item with 98(_) to each other and make a number
-    code = '98'.join(ascii_list)
+    # join list item with 256 to each other and make a number
+    code = '256'.join(ascii_list)
     number = int(code)
 
     output = coder_number(number)
@@ -20,13 +20,13 @@ def coder_number(number):
     output = output * output
     output = output *  7
     output = output - 34
-    output = output *3
-    output = output -3
+    output = output * 3
+    output = output - 3
     output = output + 6
     output = output  * 6
     output = output  / 300
-    output = output  / 1532
+    output = output  / 332
     output = output  - 1532
-    output = output *2
-    output = output  /5324
+    output = output * 2
+    output = output / 31
     return output
